c3fd9e21e7eefab961df6f7cc381eb5c6ffc821d
Daniel Esteban Osorio David <desarrollo6@historyclinical.com>
2024-10-08 18:14:03 -0500

    base url para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Api-Rick-and-Morty/',
+  base: 'https://da237.github.io/Api-Rick-and-Morty/',
   plugins: [vue()],
 })
